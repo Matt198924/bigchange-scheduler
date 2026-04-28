479353a114155c428e9332ee6131e812e054aa78
Matt198924 <m.hale@randsservice.com>
2026-04-28 15:12:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -133,6 +133,10 @@ def format_job(j):
 def index():
     return send_from_directory('static', 'index.html')
 
+@app.route('/logo.png')
+def logo():
+    return send_from_directory('static', 'logo.png')
+
 @app.route('/api/status')
 def api_status():
     try:
